4ac755abb18614bd6156f3e5ab6cf1b4afa7c8c6
CannoliBroth <brorpei@gmail.com>
2025-05-22 01:49:02 -0400

    Changed to rigidbody bullet

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,11 +1,11 @@
 extends Node3D
 
-const SPEED = 30
+const SPEED = 60
 
-@onready var mesh = $Slug
-@onready var ray = $RayCast3D
-@onready var metal_particles = $MetalParticles
-@onready var blood_particles = $BloodParticles
+@onready var mesh = $RigidBody3D/Slug
+@onready var hitbox = $RigidBody3D
+@onready var metal_particles = $RigidBody3D/MetalParticles
+@onready var blood_particles = $RigidBody3D/BloodParticles
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,22 +14,22 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	position += transform.basis * Vector3(0,0,-SPEED) * delta
-	if ray.is_colliding():
-		print("Bullet hit!")
-		mesh.visible = false
-		if ray.get_collider().is_in_group("deer"):
+	if hitbox.get_contact_count() == 1:
+		var hitobjects = hitbox.get_colliding_bodies()
+		if hitobjects[0].is_in_group("deer"):
 			blood_particles.emitting = true
 		else:
 			metal_particles.emitting = true
-		ray.enabled = false
+		print(hitobjects[0])
+		mesh.visible = false
 		await get_tree().create_timer(1.0).timeout
 		queue_free()
-
-
-
+		
 
 func _on_timer_timeout() -> void:
 	print("MISS!")
 	queue_free()
+
+	
