4ba4d029a41e3f4f9e94a5fb7c4b70548dfea86d
kongshijatwit <JKongShi@gmail.com>
2025-05-22 12:13:29 -0400

    Deer gets leashed when walking out of bounds

--- scripts/deer/deer.gd
+++ scripts/deer/deer.gd
@@ -1,5 +1,7 @@
 extends CharacterBody3D
 
+@export var leash: Marker3D
+
 enum STATE {IDLE = 0, WALK, GRAZE, SPOOK, RUN, DEAD}
 const MIN_IDLE_TIME: float = 2.0
 const MAX_IDLE_TIME: float = 2.5
@@ -19,7 +21,6 @@ var run_direction: Vector3
 var walk_speed: float = 100.0
 var run_speed: float = 200
 
-# deer should despawn after entering certain zone
 
 func _ready():
     idle_timer = randf_range(MIN_IDLE_TIME, MAX_IDLE_TIME)
@@ -31,32 +32,31 @@ func _ready():
 func _process(delta: float) -> void:
     match current_state:
         STATE.IDLE:
-            # 33% chance it walks
-            # 33% chance to graze
-            # 33% chance it continues idling
-            print("idle")
             if idle_timer > 0:
                 idle_timer -= delta
             else:
                 current_state = (randi() % 3) as STATE
                 if current_state == STATE.IDLE:
-                    print("keep idling")
+                    print("keep idling: idle -> idle")
                 elif current_state == STATE.WALK:
                     rotate_y(deg_to_rad(randf_range(0.0, 360.0)))
+                    print("going to walk state: idle -> walk")
+                elif current_state == STATE.GRAZE:
+                    print("going to graze state: idle -> graze")
                 idle_timer = randf_range(MIN_IDLE_TIME, MAX_IDLE_TIME)
             
         STATE.WALK:
-            print("walking")
             if walk_timer > 0:
                 walk_timer -= delta
             else:
                 current_state = STATE.IDLE
                 walk_timer = randf_range(MIN_WALK_TIME, MAX_WALK_TIME)
                 velocity = Vector3.ZERO
+                print("going to idle state: walk -> idle")
 
         STATE.GRAZE:
-            print("grazing")
             current_state = STATE.IDLE
+            print("going to idle state: graze -> idle")
 
         STATE.SPOOK:
             print("spooking")
@@ -65,35 +65,43 @@ func _process(delta: float) -> void:
             else:
                 current_state = STATE.RUN
                 react_timer = MAX_REACT_TIME
+                print("going to run state: spook -> run")
 
         STATE.RUN:
-            print("running")
             if run_timer > 0:
                 run_timer -= delta
             else:
                 current_state = STATE.IDLE
                 run_timer = MAX_RUN_TIME
                 velocity = Vector3.ZERO
+                print("going to idle state: run -> idle")
         
 
 
 func _physics_process(delta: float) -> void:
     if current_state == STATE.RUN:
-        # var direction: Vector3 = -(spook_object_position - global_position).normalized()
-        # direction.y = 0
         velocity = run_direction * run_speed * delta
 
     elif current_state == STATE.WALK:
-        velocity = transform.basis.z * walk_speed * delta
+        velocity = transform.basis.z * walk_speed * delta  # Walk forward in local space
         
     move_and_slide()
 
 
 func _on_area_entered(area: Area3D) -> void:
+    # Getting object-entered position for run direction
     spook_object_position = area.get_parent().position
     run_direction = -(spook_object_position - global_position).normalized()
+
+    if area.is_in_group("bound"):
+        run_direction = (leash.position - global_position).normalized()
+        spook_object_position = -leash.position
+        
     run_direction.y = 0
+
+    # Look in running direction
     look_at(spook_object_position)
     rotation.x = rad_to_deg(0)
+
+    # Set state
     current_state = STATE.SPOOK
-    # area.get_parent().queue_free()
