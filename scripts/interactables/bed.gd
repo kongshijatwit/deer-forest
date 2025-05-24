014f34c32ae95a2e50678c060e2cfc744cbf5d44
kongshijatwit <JKongShi@gmail.com>
2025-05-24 00:05:40 -0400

    Gamemanager emits reset signal after player interacts with bed

--- scripts/interactables/bed.gd
+++ scripts/interactables/bed.gd
@@ -1,2 +1,23 @@
 extends Interactable
 
+signal sleep()
+
+const SLEEP_PROMPT: String = "Go to sleep"
+var can_use: bool = true
+
+func _ready() -> void:
+	# TODO: Take in another signal that resets `can_use`
+	prompt_message = SLEEP_PROMPT
+	interacted.connect(on_bed_interacted)
+
+func on_bed_interacted(body):
+	if can_use:
+		can_use = false
+		sleep.emit()
+		prompt_message = ""
+	# Maybe call a body.reset() function in the player that interacts with the bed to reset bullets?
+	print("yeah")
+
+func allow_sleep():
+	can_use = true
+	prompt_message = SLEEP_PROMPT
--- scripts/managers/game_manager.gd
+++ scripts/managers/game_manager.gd
@@ -1,30 +1,35 @@
 extends Node3D
 
-# track "months" spent
 # [x] Create bed interactable object
-# [] Interacting will increment months : PROBLEM - Player can spam-click bed
-# [] Fade to black -> Increment month and respawn deer + monsters
+# [x] Need to get bed emitted signal when player interacts with bed, call start_new_month()
+# [ ] Fade to black -> Increment month and respawn deer + monsters -> "A new month has begun, bringing more deer"
 #
-# reset/track deer and monster spawn
 
 # Questions:
 #  - How many total months?
 #  - Do bullets correlate to amount of deer/monsters
 
-const BED_PATH: String = "bed"
+signal reset
+const BED_STRING: String = "bed"
+var month: int = 0
+
 
 func _ready() -> void:
 	print("this is your president speaking")
-	var bed_var: Node3D = $"../".find_child(BED_PATH)
-	if bed_var == null:
-		push_warning("no cool var but that's okay")
+	var bed_node: Node3D = $"../".find_child(BED_STRING)
+	if bed_node == null:
+		push_warning("no bed found but that's okay")
 	else:
-		bed_var.interacted.connect(on_new_month)
+		bed_node.sleep.connect(start_new_month)
 
 
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("ui_down"):
+		print(month)
 
-func on_new_month(body) -> void:
-	print("a new month has passed")
+
+func start_new_month() -> void:
+	# reset monster/deer spawns
+	reset.emit()
+	month += 1
 
