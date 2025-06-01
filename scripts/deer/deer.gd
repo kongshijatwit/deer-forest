63d7a9ce42e854aaf8bb445f636a5c73bc912e44
kongshijatwit <JKongShi@gmail.com>
2025-06-01 13:05:19 -0400

    Deer detects player

--- scripts/deer/deer.gd
+++ scripts/deer/deer.gd
@@ -1,8 +1,17 @@
 extends CharacterBody3D
 
-@export var leash: Marker3D
+#region Variables
 
+# Gamemanager Reference
+const GAMEMANAGER_NAME: String = "game_manager"
+var gamemanager_node: Node = null
+
+# State Machine
 enum STATE {IDLE = 0, ROTATE, WALK, GRAZE, SPOOK, RUN, DEAD}
+var current_state := STATE.IDLE
+var spook_object_position := Vector3.ZERO
+
+# Timers
 const MIN_IDLE_TIME: float = 2.0
 const MAX_IDLE_TIME: float = 2.5
 const ROTATE_TIME: float = 5.0
@@ -10,39 +19,43 @@ const MAX_RUN_TIME: float = 5.0
 const MAX_REACT_TIME: float = 0.35
 const MIN_WALK_TIME: float = 1.0
 const MAX_WALK_TIME: float = 3.5
-const GAMEMANAGER_NAME: String = "game_manager"
-const BOUNDARY_GROUP: String = "boundary"
-const BULLET_GROUP: String = "bullet"
-
-var gamemanager_node: Node = null
-var dummy_prefab: PackedScene = load("res://prefabs/skeleton/ragdoll_skeleton_test.tscn")
-
-#Deer Sounds
-@onready var deer_sfx = $AudioStreamPlayer3D
-var deer_sfx_lib = ["res://assets/audio/sfx/deer/A_Deer-001.ogg", "res://assets/audio/sfx/deer/A_Deer-002.ogg", "res://assets/audio/sfx/deer/A_Deer-003.ogg",
-"res://assets/audio/sfx/deer/A_Deer-004.ogg", "res://assets/audio/sfx/deer/A_Deer-005.ogg"]
-
-var spook_object_position := Vector3.ZERO
-var current_state := STATE.IDLE
 var idle_timer: float
 var react_timer: float
 var walk_timer: float
 var run_timer: float
 
+# Collision Groups
+const BOUNDARY_GROUP: String = "boundary"
+const BULLET_GROUP: String = "bullet"
+const PLAYER_GROUP: String = "player"
+
+# Positioning & Speed
+const WALK_SPEED: float = 100.0
+const RUN_SPEED: float = 300.0
+var original_position: Vector3
 var run_direction: Vector3
-var walk_speed: float = 100.0
-var run_speed: float = 200
 var rotate_angle: float
+
+# Ragdoll - NOTE: Might replace with skeleton features instead of dummy spawning
+var dummy_prefab: PackedScene = load("res://prefabs/skeleton/ragdoll_skeleton_test.tscn")
+
+# Deer Sounds
+@onready var deer_sfx = $AudioStreamPlayer3D
+var deer_sfx_lib = ["res://assets/audio/sfx/deer/A_Deer-001.ogg", "res://assets/audio/sfx/deer/A_Deer-002.ogg", "res://assets/audio/sfx/deer/A_Deer-003.ogg", "res://assets/audio/sfx/deer/A_Deer-004.ogg", "res://assets/audio/sfx/deer/A_Deer-005.ogg"]
 
 # Debug constants
 const DEBUG_MODE: bool = true
 const KILL_DEER_INPUT: String = "ui_up"
 const RESET_DEER_INPUT: String = "ui_down"
 
+#endregion
+
 
 func _ready():
 	reset_all_timers()
 	add_gamemanager_signal()
+	original_position = position
+	$detection.body_entered.connect(_on_body_entered)
 
 
 func _process(delta: float) -> void:
@@ -112,16 +125,17 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	if current_state == STATE.RUN:
-		velocity = run_direction * run_speed * delta
+		velocity = run_direction * RUN_SPEED * delta
 
 	elif current_state == STATE.WALK:
-		velocity = transform.basis.z * walk_speed * delta  # Walk forward in local space
+		velocity = transform.basis.z * WALK_SPEED * delta  # Walk forward in local space
 		
 	move_and_slide()
 
 
+#region State Functions
+
 func kill_deer() -> void:
-	# await get_tree().create_timer(0.01).timeout
 	velocity = Vector3.ZERO
 	var previous_state: STATE = current_state  # DEBUG VAR
 	current_state = STATE.DEAD
@@ -147,32 +161,6 @@ func set_active(active: bool) -> void:
 			n.set_deferred("disabled", !active)
 		n.visible = active
 
-func spawn_dummy():
-	var dummy = dummy_prefab.instantiate()
-	dummy.position.y = -2
-	dummy.get_node("CollisionShape3D").disabled = true
-	add_child(dummy)
-	# add timer for despawn
-
-
-#region Debug Functions
-
-func debug_deer_print(message: String) -> void:
-	if DEBUG_MODE:
-		print(message)
-
-func debug_state_change(prev: STATE) -> void:
-	if DEBUG_MODE:
-		print("changing state: " + STATE.keys()[prev] + " -> " + STATE.keys()[current_state])
-		if current_state == STATE.DEAD:
-			print("dead")
-
-func debug_inputs() -> void:
-	if Input.is_action_just_pressed(RESET_DEER_INPUT) && current_state == STATE.DEAD:
-		reset_deer()
-	elif Input.is_action_just_pressed(KILL_DEER_INPUT) && current_state != STATE.DEAD:
-		kill_deer()
-
 #endregion
 
 
@@ -180,24 +168,19 @@ func debug_inputs() -> void:
 
 func _on_area_entered(area: Area3D) -> void:
 	var previous_state: STATE = current_state
+	print("spooked by: " + area.get_parent().name)
 
 	if area.is_in_group(BULLET_GROUP):
 		spook_object_position = area.get_parent().position
-		run_direction = -(spook_object_position - global_position).normalized()
-		run_direction.y = 0
-		look_at(spook_object_position)
-		rotation.x = rad_to_deg(0)
-		current_state = STATE.SPOOK
-		debug_state_change(previous_state)
+		spooked_by_object(spook_object_position, true)
 
 	if area.is_in_group(BOUNDARY_GROUP):
-		run_direction = (leash.position - global_position).normalized()
-		spook_object_position = -leash.position
-		run_direction.y = 0
-		look_at(spook_object_position)
-		rotation.x = rad_to_deg(0)
-		current_state = STATE.SPOOK
-		debug_state_change(previous_state)
+		spook_object_position = -original_position
+		spooked_by_object(spook_object_position, false)
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group(PLAYER_GROUP) or body.name == "Player":
+		spooked_by_object(body.position, true)
 
 func on_head_hit(area: Area3D) -> void:
 	if area.is_in_group(BULLET_GROUP):
@@ -231,5 +214,51 @@ func reset_all_timers() -> void:
 	react_timer = MAX_REACT_TIME
 	walk_timer = randf_range(MIN_WALK_TIME, MAX_WALK_TIME)
 	run_timer = MAX_RUN_TIME
+
+#endregion
+
+
+#region Helper Functions
+
+func spooked_by_object(object_position: Vector3, run_away: bool):
+	run_direction = (object_position - global_position)
+	run_direction = run_direction.normalized()
+	if run_away: run_direction *= -1
+	run_direction.y = 0
+	look_at(object_position)
+	rotation.x = rad_to_deg(0)
+
+	# Debug
+	var previous_state = current_state
+	current_state = STATE.SPOOK
+	debug_state_change(previous_state)
+
+func spawn_dummy():
+	var dummy = dummy_prefab.instantiate()
+	dummy.position.y = -2
+	dummy.get_node("CollisionShape3D").disabled = true
+	add_child(dummy)
+	# TODO: add timer for despawn
+
+#endregion
+
+
+#region Debug Functions
+
+func debug_deer_print(message: String) -> void:
+	if DEBUG_MODE:
+		print(message)
+
+func debug_state_change(prev: STATE) -> void:
+	if DEBUG_MODE:
+		print("changing state: " + STATE.keys()[prev] + " -> " + STATE.keys()[current_state])
+		if current_state == STATE.DEAD:
+			print("dead")
+
+func debug_inputs() -> void:
+	if Input.is_action_just_pressed(RESET_DEER_INPUT) && current_state == STATE.DEAD:
+		reset_deer()
+	elif Input.is_action_just_pressed(KILL_DEER_INPUT) && current_state != STATE.DEAD:
+		kill_deer()
 
 #endregion
