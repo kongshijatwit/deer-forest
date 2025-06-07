cc6712b93c3b0a35610d694fd9e0533e1bb2adc9
kongshijatwit <JKongShi@gmail.com>
2025-06-07 12:41:09 -0400

    Finish adding beginning dialog

--- scripts/interactables/dialog_blocker.gd
+++ scripts/interactables/dialog_blocker.gd
@@ -15,21 +15,23 @@ func _ready():
 
 
 func begin_dialog(_body):
+	talking.emit()
 	if finished_talking:
 		prompt_message = ""
 		if !GlobalVariables.villager_talked:
+			MadTalkGlobals.set_variable("villager_reputation", GlobalVariables.villager_reputation)
 			GlobalVariables.villager_talked = true
 			finished_talking = false
 			dialog_controller.start_dialog("villager")
 		elif !GlobalVariables.cultist_talked:
 			MadTalkGlobals.set_variable("cultist_reputation", GlobalVariables.cultist_reputation)
-			print(MadTalkGlobals.get_variable("cultist_reputation"))
 			GlobalVariables.cultist_talked = true
 			finished_talking = false
 			dialog_controller.start_dialog("cultist")
 	
 
 func finish_dialog(_sheet_name: Variant, _sequence_id: Variant):
+	done_talking.emit()
 	prompt_message = TALK_PROMPT
 	finished_talking = true
 	if GlobalVariables.villager_talked and GlobalVariables.cultist_talked:
@@ -38,4 +40,6 @@ func finish_dialog(_sheet_name: Variant, _sequence_id: Variant):
 
 
 func reset_dialog_blocker():
-	pass
+	$CollisionShape3D.disabled = false
+	visible = true
+
