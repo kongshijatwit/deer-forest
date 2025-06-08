a38f0ba5f7338c8ba10ed68c25850bad270a04db
CannoliBroth <brorpei@gmail.com>
2025-06-08 03:30:20 -0400

    Added cabin feet sfx and knocking

--- scripts/interactables/door.gd
+++ scripts/interactables/door.gd
@@ -2,18 +2,35 @@ extends Interactable
 
 @export var ray_path := "/root/Main/NavigationRegion3D/Player/GroundRay"
 @onready var door_sfx = $"../AudioStreamPlayer3D"
+@onready var timer = $"../Timer"
 
 var playback : AnimationNodeStateMachinePlayback
 var is_open := false
-var door_sfx_lib = ["res://assets/audio/sfx/door/A_Door_Close.ogg", "res://assets/audio/sfx/door/A_Door_Open.ogg"]
+var door_sfx_lib = ["res://assets/audio/sfx/door/A_Door_Close.ogg", "res://assets/audio/sfx/door/A_Door_Open.ogg", "res://assets/audio/sfx/door/A_Door_Knock.ogg"]
 var ambiance_bus = AudioServer.get_bus_index("Ambiance")
 var groundray
+var talked = false
+var knocked = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	playback = $AnimationTree.get("parameters/playback")
 	# groundray = get_node(ray_path)
 	groundray = get_node("../%player/GroundRay")
+	
+func _process(delta) -> void:
+	if !is_open and !talked and !knocked:
+		timer.wait_time = randf_range(3.0,5.0)
+		timer.start()
+		knocked = true
+	if timer.get_time_left() < 1 and knocked:
+		timer.stop()
+		if !is_open:
+			door_sfx.stream = load(door_sfx_lib[2])
+			door_sfx.play()
+		knocked = false
+			
 
 func toggle(_body):
 	is_open = not is_open
@@ -36,3 +53,5 @@ func _shelter():
 			print("NO WIND!")
 			AudioServer.set_bus_effect_enabled(ambiance_bus, 0, true)
 		
+#func _on_dialog_blocker_talking() -> void:
+#	talked = true
