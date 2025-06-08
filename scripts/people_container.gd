c87c49733938a4851154738e909af463dfbbc4d0
kongshijatwit <JKongShi@gmail.com>
2025-06-08 14:33:20 -0400

    Finish cultist/villager swapout

--- scripts/people_container.gd
+++ scripts/people_container.gd
@@ -3,20 +3,27 @@ extends Node3D
 
 @onready var dialog_blocker = %dialog_blocker
 var can_move: bool = false
+var first_convo: bool = false
 var move_amount: float
 
 
 func _ready():
 	dialog_blocker.done_talking.connect(start_lerp)
-	move_amount = position.x + 10.0
+	move_amount = position.x + 5.3
 
 
 func _process(delta):
 	if can_move:
-		lerp(position.x, move_amount, delta)
-		if position.x == move_amount:
+		print("can_move")
+		# position.x = lerp(position.x, move_amount, delta)
+		position.x += 10.0 * delta
+		if position.x > move_amount:
 			can_move = false
+			if first_convo:
+				visible = false
 	
 
 func start_lerp():
+	first_convo = true
+	move_amount = position.x + 5.3
 	can_move = true
