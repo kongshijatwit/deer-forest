6f618901b4475730aa3beb6eba32a5ccd3a4f48d
kongshijatwit <JKongShi@gmail.com>
2025-06-08 17:47:01 -0400

    Switch to animation player for moving people

--- scripts/people_container.gd
+++ scripts/people_container.gd
@@ -1,36 +1,23 @@
 extends Node3D
 
-
 @onready var dialog_blocker = %dialog_blocker
 @onready var game_manager = %game_manager
-
-const MOVE_AMOUNT: float = 5.3
-var can_move: bool = false
-var first_convo: bool = false
-var new_x_position: float
+@onready var animation_player = $AnimationPlayer
+var first_convo_done: bool = false
 
 
 func _ready():
-	dialog_blocker.done_talking.connect(start_lerp)
+	dialog_blocker.done_talking.connect(move_people)
 	game_manager.reset.connect(reset_people)
-	new_x_position = position.x + MOVE_AMOUNT
 
-func _process(delta):
-	if can_move:
-		position.x += 10.0 * delta
-		if position.x > new_x_position:
-			can_move = false
-			if first_convo:
-				visible = false
-			else:
-				first_convo = true
-	
-func start_lerp():
-	new_x_position = position.x + MOVE_AMOUNT
-	can_move = true
+func move_people():
+	if !first_convo_done:
+		animation_player.play("move_villager")
+		first_convo_done = true
+	else:
+		animation_player.play("move_cultist")
 
 func reset_people():
-	visible = true
-	first_convo = false
-	position.x = 0
+	first_convo_done = false
+	animation_player.play("RESET")
 
