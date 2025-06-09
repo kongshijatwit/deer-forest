258fc76eaeafac4081cb259452be1029ed4e6282
CannoliBroth <brorpei@gmail.com>
2025-06-09 01:22:01 -0400

    Finished healthbar

--- scripts/ui/player_hud.gd
+++ scripts/ui/player_hud.gd
@@ -2,7 +2,7 @@ extends Control
 
 @onready var ammo = $BulletCount
 @onready var player = $"../.."
-@onready var ammo_timer = $BulletCount/NoAmmo
+@onready var health_bar = $HealthBar
 
 var timer_start = false
 var timer_fin = true
@@ -15,5 +15,12 @@ func _process(delta):
 	ammo_hud = str(player.bullet_count) + "/" + str(player.bullet_reserve)
 	ammo.bbcode_text = ammo_hud
 	if player.bullet_count == 0 and player.bullet_reserve == 0:
-		ammo_timer.start()
 		ammo.bbcode_text = "[color=#ee0000]%s[/color]" % ammo_hud
+	health_bar.value = player.HEALTH
+	if player.gun_equipped:
+		health_bar.visible = true
+		ammo.visible = true
+	else:
+		health_bar.visible = false
+		ammo.visible = false
+	
