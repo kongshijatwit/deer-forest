c35dc02fa413bffeb6206890fa6df33d6f5baf82
kongshijatwit <JKongShi@gmail.com>
2025-06-08 20:19:41 -0400

    Able to switch to dark music on entering deep forest

--- scripts/environment/dim_zone.gd
+++ scripts/environment/dim_zone.gd
@@ -13,11 +13,13 @@ func _ready() -> void:
 	if world_env == null: push_error("not found")
 
 func zone_entered(body: Node3D) -> void:
-	if body.is_in_group("player") or body.name == "player": 
+	if body.is_in_group("player") or body.name == "player":
+		$"../music_player/dark_music".play()
 		set_fog(true)
 
 func zone_exited(body: Node3D) -> void:
-	if body.is_in_group("player") or body.name == "player": 
+	if body.is_in_group("player") or body.name == "player":
+		$"../music_player/dark_music".stop()
 		set_fog(false)
 
 func set_fog(active: bool) -> void:
