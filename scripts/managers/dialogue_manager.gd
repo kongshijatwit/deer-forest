4f9735ce40d2b1831b173c345e491c944447fd43
kongshijatwit <JKongShi@gmail.com>
2025-06-15 16:40:01 -0400

    Interact key can advance dialog

--- scripts/managers/dialogue_manager.gd
+++ scripts/managers/dialogue_manager.gd
@@ -1,7 +1,8 @@
 extends Node
 
 const ADVANCE_KEY: String = "ui_accept"
+const INTERACT_KEY: String = "interact"
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed(ADVANCE_KEY):
+	if Input.is_action_just_pressed(ADVANCE_KEY) or Input.is_action_just_pressed(INTERACT_KEY):
 		$MadTalk.dialog_acknowledge()
