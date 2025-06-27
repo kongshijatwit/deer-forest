eb3ab726c8287abf390d47412476725a77c1afd0
kongshijatwit <JKongShi@gmail.com>
2025-06-27 17:38:28 -0400

    Add timer for delay when going to next objective

--- scripts/ui/objectives.gd
+++ scripts/ui/objectives.gd
@@ -1,13 +1,15 @@
 extends Control
 
-# Objectives
-# --> Talk to strangers
-# --> Pick up gun
-# --> Hunt 15 deer OR Collect Artifact
 
+const DISPLAY_TIME_IN_SECONDS: float = 2.0
+
+# References to child objects
 @onready var objective1_checkbox: CheckBox = $"objective_container/objective_1"
 @onready var objective2_checkbox: CheckBox = $"objective_container/objective_2"
+@onready var or_text: RichTextLabel = $"objective_container/or_text"
+@onready var timer: Timer = $"Timer"
 
+# References to objectives signals
 @onready var dialog_blocker = $"../../%dialog_blocker"
 @onready var game_manager = $"../../%game_manager"
 @onready var player = $"../../%player"
@@ -17,71 +19,74 @@ extends Control
 # Maybe have an array of objective strings and create/modify checkboxes at each index
 # -- index gets incremented when an objective is completed
 
-const OBJECTIVES: Array[String] = ["Talk to stranger", "Talk to next stranger", "Pick up gun", "Hunt 15 deer", "Collect artifact", "Go to bed"]
+# Objectives variables
+enum objectives {STRANGER_1 = 0, STRANGER_2, GUN, DEER, ARTIFACT, BED}
+var objective_dict: Dictionary = {
+	objectives.STRANGER_1: ["Talk to stranger", false],
+	objectives.STRANGER_2: ["Talk to next stranger", false],
+	objectives.GUN: ["Pick up gun", false],
+	objectives.DEER: ["Pick up gun", false],
+	objectives.ARTIFACT: ["Pick up gun", false],
+	objectives.BED: ["Pick up gun", false]
+}
 var objective_pointer_1: int = 0
 var objective_pointer_2: int = 4
 
-# objectives (might not need)
-var talked_to_stranger_1: bool = false
-var talked_to_stranger_2: bool = false
-var picked_up_gun: bool = false
-var finish_deer: bool = false
-var finish_artifact: bool = false
-var sleeping: bool = false
-
 
 func _ready():
+	timer.wait_time = DISPLAY_TIME_IN_SECONDS
 	dialog_blocker.done_talking.connect(update_talking_objective)
 	game_manager.quest_complete.connect(update_hunt_objective)
 	bed.sleep.connect(update_bed_objective)
 	game_manager.reset.connect(reset_objectives)
+	timer.timeout.connect(check_next_objective)
+	update_objective_panel()
 
 
 func update_talking_objective():
-	objective_pointer_1 += 1
-	update_objective_panel()
-	# if talked_to_stranger_1:
-	# 	talked_to_stranger_2 = true
-	# else:
-	# 	talked_to_stranger_1 = true
-	
+	if objective_dict[objectives.STRANGER_1][1]:
+		objective_dict[objectives.STRANGER_2][1] = true
+	else:
+		objective_dict[objectives.STRANGER_1][1] = true
+	check_objective()
 
 
 func update_gun_objective():
-	objective_pointer_1 += 1
-	update_objective_panel()
-	picked_up_gun = true
+	objective_dict[objectives.GUN][1] = true
+	check_objective()
 
 
 func update_hunt_objective():
-	if GlobalVariables.deer_killed >= game_manager.REQUIRED_DEER_AMOUNT:
-		finish_deer = true
-		objective_pointer_1 = OBJECTIVES.back()
-		update_objective_panel()
-	elif GlobalVariables.artifact_piece_collected:
-		objective_pointer_2 = OBJECTIVES.back()
-		update_objective_panel()
-		finish_artifact = true
+	objective_dict[objectives.DEER][1] = true
+	check_objective()
+
 
 func update_bed_objective():
-	sleeping = true
+	objective_dict[objectives.BED][1] = true
+	check_objective()
+
 
 func reset_objectives():
-	talked_to_stranger_1 = false
-	talked_to_stranger_2 = false
-	picked_up_gun = false
-	finish_deer = false
-	finish_artifact = false
-	sleeping = false
 	objective_pointer_1 = 0
 	objective_pointer_2 = 4
 
+
+func check_objective():
+	if objective_dict[objective_pointer_1 as objectives][1]:
+		objective1_checkbox.button_pressed = true
+		timer.start()
+
+
+func check_next_objective():
+	objective_pointer_1 += 1
+	if objective_pointer_1 > 3:
+		objective_pointer_1 = 5
+	update_objective_panel()
+	check_objective()
+	
+
 func update_objective_panel():
-	objective1_checkbox.text = OBJECTIVES[objective_pointer_1]
-	# objective2_checkbox.text = OBJECTIVES[objective_pointer_2]
-	if objective_pointer_1 == 3:
-		objective2_checkbox.visible = true
-
-
+	objective1_checkbox.button_pressed = false
+	objective1_checkbox.text = objective_dict[objective_pointer_1 as objectives][0]
 
 
